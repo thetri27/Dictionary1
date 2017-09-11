0aabdf530cc8792d6111a123a2f6a56fe576e325
thetri27 <thetri270992@gmail.com>
2017-09-11 08:45:01 +0700

    commit change

---
+++ app/src/main/java/thetri/com/dictionary/loginActivity.java
@@ -0,0 +1,10 @@
+package thetri.com.dictionary;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DELL on 9/11/2017.
+ */
+
+public class loginActivity extends AppCompatActivity{
+}
